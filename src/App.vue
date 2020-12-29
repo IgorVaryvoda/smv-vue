59e7e1fb421300e1919c6ed8e8d1b9ab9b403900
Igor Varyvoda <cheguevaraua@gmail.com>
2020-12-29 20:43:27 +0100

    hey

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="center">
+      <img alt="Vue logo" src="./assets/logo.png"> <span style="font-size:122px;padding: 0 20px;">+</span> <img src="https://sirv-cdn.sirv.com/website/img/sirv-logo.svg" width="250px"/>
+    </div>
+    <HelloWorld msg="Here's a demo of SMV in VueJS 2"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  head: {
+  title: {
+    inner: 'Sirv with SMV is dope as fuck'
+  },
+  script: [
+      { type: 'text/javascript', src: 'https://scripts.sirv.com/sirvjs/v3/sirv.js'}
+    ]
+  },
+mounted() {
+  // let smv = document.createElement('script')
+  // smv.setAttribute('src', 'https://scripts.sirv.com/sirvjs/v3/sirv.js')
+  // document.head.appendChild(smv)
+  Sirv.start()
+}
 }
 </script>
 
@@ -24,5 +39,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  border: 3px solid green;
 }
 </style>
