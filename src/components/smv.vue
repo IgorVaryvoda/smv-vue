7e9c1fadef00181691bd55aba23ef20743cb7acd
Igor Varyvoda <cheguevaraua@gmail.com>
2020-12-30 11:22:38 +0100

    better button style

--- src/components/smv.vue
+++ src/components/smv.vue
@@ -9,7 +9,8 @@
   <div data-src="https://demo.sirv.com/video.mp4"></div>
 </div>
 <h3>Autostart OFF</h3>
-<button v-on:click="startSirv">Start</button>
+<p>Sirv Media Viewer will not initialize until you push this beautiful button below.</p>
+<button class="glow-on-hover" v-on:click="startSirv">Start</button>
 <div class="Sirv off" data-options="autostart:off">
   <div data-src="https://demo.sirv.com/example.spin"></div>
   <div data-src="https://demo.sirv.com/image.jpg" data-type="zoom"></div>
@@ -48,13 +49,15 @@ export default {
     })
     },
     startSirv() {
-      if (typeof window.Sirv === 'undefined') {
-        this.loadScript('https://scripts.sirv.com/sirvjs/v3/sirv.js').then(() => {
-          window.Sirv.start('.off')
-        })
-      } else {
-        window.Sirv.start('.off')
-      }
+      typeof window.Sirv === 'undefined' ? this.loadScript('https://scripts.sirv.com/sirvjs/v3/sirv. js').then(() => {
+        window.Sirv.start('.off') }) : window.Sirv.start('.off')
+      // if (typeof window.Sirv === 'undefined') {
+      //   this.loadScript('https://scripts.sirv.com/sirvjs/v3/sirv.js').then(() => {
+      //     window.Sirv.start('.off')
+      //   })
+      // } else {
+      //   window.Sirv.start('.off')
+      // }
     },
     previous() {
       if (window.Sirv.viewer.getInstance('.buttons').isReady()) {
@@ -89,5 +92,64 @@ a {
 .container {
   max-width:750px;
   margin: 0 auto;
+}
+.glow-on-hover {
+    width: 220px;
+    height: 50px;
+    border: none;
+    outline: none;
+    color: #fff;
+    background: #111;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    border-radius: 10px;
+}
+
+.glow-on-hover:before {
+    content: '';
+    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+    position: absolute;
+    top: -2px;
+    left:-2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 0;
+    transition: opacity .3s ease-in-out;
+    border-radius: 10px;
+}
+
+.glow-on-hover:active {
+    color: #000
+}
+
+.glow-on-hover:active:after {
+    background: transparent;
+}
+
+.glow-on-hover:hover:before {
+    opacity: 1;
+}
+
+.glow-on-hover:after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+}
+
+@keyframes glowing {
+    0% { background-position: 0 0; }
+    50% { background-position: 400% 0; }
+    100% { background-position: 0 0; }
 }
 </style>
